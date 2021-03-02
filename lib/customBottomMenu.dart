0b7ff62c5ac0cfcfbd10283035f8b65e92a2f0ef
Abdullah <abdullahozvanli@gmail.com>
2021-03-03 00:34:38 +0300

    Edit&Create=customBottomMenu

---
+++ lib/customBottomMenu.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'listPage.dart';
+
+// import 'a_tab_view.dart';
+// import 'b_tab_view.dart';
+// import 'c_tab_view.dart';
+
+class BnbDemo extends StatefulWidget {
+  @override
+  _BnbDemoState createState() => _BnbDemoState();
+}
+
+class _BnbDemoState extends State<BnbDemo> {
+  /// [1] BottomNavigationBar'da yer alacak sayfaların listesi
+  final _currentTabPages = <Widget>[
+    MenuText("PROFİLİM SAYFASI"),
+    MenuText("MESAJLARIM SAYFASI"),
+    ListPage(),
+    MenuText("ARAMA SAYFASI"),
+    MenuText("MENU SAYFASI"),
+  ];
+
+  /// [2] Sayfanın indisini tutuyor.
+  int _currentIndex = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildScaffold(context);
+  }
+
+  Scaffold _buildScaffold(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildSafeArea(),
+      bottomNavigationBar: _buildBottomNavigationBar(context),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: Text('BNB Demo'),
+    );
+  }
+
+  SafeArea _buildSafeArea() {
+    return SafeArea(
+      /// [3] BottomNavigationBar'da [1]'deki sayfalardan gösterilecek olanı
+      /// currentIndex değerine göre belirliyoruz
+      child: _currentTabPages[_currentIndex],
+    );
+  }
+
+  BottomNavigationBar _buildBottomNavigationBar(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+
+      /// [4] Geçerli olan indis
+      currentIndex: _currentIndex,
+      selectedItemColor: Theme.of(context).accentColor,
+
+      /// [5] BNB'de seçilen sekmeye göre ne olacağını ayarlıyoruz
+      onTap: _onTap,
+
+      /// [6] Sekmede yerleştirilecek olan sayfaları temsil eden tıklama alanı
+      items: _items,
+    );
+  }
+
+  void _onTap(selectedIndex) {
+    /// Seçilen(selectedIndex) ile Görünen (_currentIndex) değer aynı
+    /// ise setState çalışmasın
+    if (selectedIndex == _currentIndex) {
+      return;
+    }
+
+    /// selectedIndex değerini [_currentIndex'e atadık]
+    setState(() {
+      debugPrint('değer: $selectedIndex');
+      _currentIndex = selectedIndex;
+    });
+  }
+
+  List<BottomNavigationBarItem> get _items {
+    return <BottomNavigationBarItem>[
+      _buildABottomNavigationBarItem(),
+      _buildBBottomNavigationBarItem(),
+      _buildCBottomNavigationBarItem(),
+      _buildDBottomNavigationBarItem(),
+      _buildEBottomNavigationBarItem(),
+    ];
+  }
+
+  BottomNavigationBarItem _buildABottomNavigationBarItem() {
+    return BottomNavigationBarItem(
+      icon: Icon(Icons.account_circle_rounded), //, color: Colors.grey, size: 30
+      label: 'Profilim',
+      // activeIcon: Icon(Icons.home),
+    );
+  }
+
+  BottomNavigationBarItem _buildBBottomNavigationBarItem() {
+    return BottomNavigationBarItem(
+      icon: Icon(Icons.message),
+      label: 'Mesajlarım',
+      // activeIcon: Icon(Icons.hotel),
+    );
+  }
+
+  BottomNavigationBarItem _buildCBottomNavigationBarItem() {
+    return BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      label: 'Ana Sayfa',
+      // activeIcon: Icon(Icons.map),
+    );
+  }
+
+  BottomNavigationBarItem _buildDBottomNavigationBarItem() {
+    return BottomNavigationBarItem(
+      icon: Icon(Icons.search),
+      label: 'Arama', //,
+      //activeIcon: Icon(Icons.menu),
+    );
+  }
+
+  BottomNavigationBarItem _buildEBottomNavigationBarItem() {
+    return BottomNavigationBarItem(
+      icon: Icon(Icons.menu),
+      label: 'Menü', //,
+      // activeIcon: Icon(Icons.menu),
+    );
+  }
+}
+
+class MenuText extends StatelessWidget {
+  final String txt;
+  MenuText(this.txt);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text(txt)),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:diyet/listPage.dart';
+import 'package:diyet/customBottomMenu.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: SafeArea(child: MyHomePage()),
     );
   }
 }
@@ -22,102 +22,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 2;
-  final List<Widget> _children = [
-    MenuText("PROFİLİM SAYFASI"),
-    MenuText("MESAJLARIM SAYFASI"),
-    ListPage(),
-    MenuText("ARAMA SAYFASI"),
-    MenuText("MENU SAYFASI"),
-  ];
   GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        key: _drawerKey,
-        drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('Drawer Header'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-            ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
+      key: _drawerKey,
+      drawer: Drawer(),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(30),
+            topLeft: Radius.circular(30),
+          ),
+          boxShadow: [
+            BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
           ],
         ),
-        ),
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-                topRight: Radius.circular(30), topLeft: Radius.circular(30)),
-            boxShadow: [
-              BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
-            ],
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
           ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30.0),
-              topRight: Radius.circular(30.0),
-            ),
-            child: BottomNavigationBar(
-              currentIndex: _currentIndex,
-              onTap: onTabTapped,
-              type: BottomNavigationBarType.fixed,
-              items: [
-                BottomNavigationBarItem(
-                  label: "Profilim",
-                  icon: new Icon(Icons.account_circle_rounded),
-                ),
-                BottomNavigationBarItem(
-                  icon: new Icon(Icons.mail),
-                  label: "Messagess",
-                ),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.home), label: "Ana Sayfa"),
-                BottomNavigationBarItem(icon: Icon(Icons.search), label: "Ara"),
-                BottomNavigationBarItem(icon: Icon(Icons.menu), label: "Menü")
-              ],
-            ),
-          ),
+          child: BnbDemo(),
         ),
-        body: _children[_currentIndex]);
-  }
-
-  void onTabTapped(int index) {
-    setState(() {
-      index == 4 ? _drawerKey.currentState.openDrawer() : _currentIndex = index;
-    });
-  }
-}
-
-class MenuText extends StatelessWidget {
-  final String txt;
-  MenuText(this.txt);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text(txt)),
+      ),
     );
   }
 }
